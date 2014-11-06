67dde231912f7d93955e58cf6af856ab17e9be81
Michal Marek <mmarek@suse.cz>
2014-11-06 16:46:18 +0100

    rpm/config.sh: Build against the SLE12 target in the OBS

--- rpm/config.sh
+++ rpm/config.sh
@@ -4,5 +4,5 @@ SRCVERSION=3.12
 VARIANT=
 # buildservice projects to build the kernel against
 # There is no SLE12 in the OBS yet, so build against 13.1 for the time being
-OBS_PROJECT=openSUSE:13.1
+OBS_PROJECT=SUSE:SLE-12:GA
 IBS_PROJECT=SUSE:SLE-12:GA
