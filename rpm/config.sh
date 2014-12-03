d5ca4ae4fe33b97874f5c264107a7ccbe7dbcf2a
Michal Marek <mmarek@suse.cz>
2014-12-03 17:10:01 +0100

    Build against the :Update project in the IBS

--- rpm/config.sh
+++ rpm/config.sh
@@ -5,4 +5,4 @@ VARIANT=
 # buildservice projects to build the kernel against
 # There is no SLE12 in the OBS yet, so build against 13.1 for the time being
 OBS_PROJECT=SUSE:SLE-12:GA
-IBS_PROJECT=SUSE:SLE-12:GA
+IBS_PROJECT=SUSE:SLE-12:Update
